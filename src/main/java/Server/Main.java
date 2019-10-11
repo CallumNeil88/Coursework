3a6782b67c5ac3c60fe57a850c1ac10526e40271
89078 <89078@farnborough.ac.uk>
2019-10-11 12:09:38 +0100

    Start of stage 2 + Added more libarys

--- src/main/java/Server/Main.java
+++ src/main/java/Server/Main.java
@@ -1,5 +1,11 @@
 package Server;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
 import org.sqlite.SQLiteConfig;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -9,9 +15,48 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static void main(String[] args) {
+
+        openDatabase("Database.db");
+
+        ResourceConfig config = new ResourceConfig();
+        config.packages("Controllers");
+        config.register(MultiPartFeature.class);
+        ServletHolder servlet = new ServletHolder(new ServletContainer(config));
+
+        Server server = new Server(8081);
+        ServletContextHandler context = new ServletContextHandler(server, "/");
+        context.addServlet(servlet, "/*");
+
+        try {
+            server.start();
+            System.out.println("Server successfully started.");
+            server.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        Scanner inputInt = new Scanner(System.in);
+        Scanner inputStr = new Scanner(System.in);
+
+        System.out.println("Enter your UserID");
+        int UserID = inputInt.nextInt();
+        System.out.println("Enter your Username");
+        String Username = inputStr.nextLine();
+        System.out.println("Enter your Password");
+        String Password = inputStr.nextLine();
+
+        writeDatabase(UserID,Username,Password);
+
+        readDatabase();
+
+        closeDatabase();
+    }
+
     public static Connection db = null;
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         openDatabase("Database.db");
         readDatabase();
 
@@ -30,7 +75,7 @@ public class Main {
         readDatabase();
 
         closeDatabase();
-    }
+    }*/
 
     private static void openDatabase(String dbFile) {
         try  {
