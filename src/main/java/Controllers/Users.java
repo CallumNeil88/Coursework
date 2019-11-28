50a69c9ea371f5c3a7de8a2a19476724194e8f2b
Callum <89078@farnborough.ac.uk>
2019-11-28 19:05:13 +0000

    Added login token

--- src/main/java/Controllers/Users.java
+++ src/main/java/Controllers/Users.java
@@ -2,16 +2,14 @@ package Controllers;
 
 import Server.Main;
 import org.glassfish.jersey.media.multipart.FormDataParam;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 
-
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.awt.*;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.UUID;
 
 @Path("users/")
@@ -43,6 +41,52 @@ public class Users {
     }
 
     @POST
+    @Path("login")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String loginUser(@FormDataParam("username") String username, @FormDataParam("password") String password) {
+
+        try {
+
+            PreparedStatement ps1 = Main.db.prepareStatement("SELECT Password FROM Users WHERE Username = ?");
+            ps1.setString(1, username);
+            ResultSet loginResults = ps1.executeQuery();
+            if (loginResults.next()) {
+
+                String correctPassword = loginResults.getString(1);
+
+                if (password.equals(correctPassword)) {
+
+                    String token = UUID.randomUUID().toString();
+
+                    PreparedStatement ps2 = Main.db.prepareStatement("UPDATE Users SET Token = ? WHERE Username = ?");
+                    ps2.setString(1, token);
+                    ps2.setString(2, username);
+                    ps2.executeUpdate();
+
+                    return "{\"token\": \""+ token + "\"}";
+
+                } else {
+
+                    return "{\"error\": \"Incorrect password!\"}";
+
+                }
+
+            } else {
+
+                return "{\"error\": \"Unknown user!\"}";
+
+            }
+
+        }catch (Exception exception){
+            System.out.println("Database error during /user/login: " + exception.getMessage());
+            return "{\"error\": \"Server side error!\"}";
+        }
+
+
+    }
+
+   /* @POST
     @Path("login")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     @Produces(MediaType.APPLICATION_JSON)
@@ -87,7 +131,7 @@ public class Users {
             return "{\"error\": \"server side error\"}";
         }
 
-    }
+    }*/
 
     @POST
     @Path("delete")
